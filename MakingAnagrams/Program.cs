6f44cf6cc8755633884ae76054fca2efe41f0d39
Srivardhan Hebbar <srhebbar@microsoft.com>
2016-10-11 13:25:25 +0530

    Working program

--- MakingAnagrams/Program.cs
+++ MakingAnagrams/Program.cs
@@ -10,6 +10,28 @@ namespace MakingAnagrams
     {
         static void Main(string[] args)
         {
+            string a = Console.ReadLine().Trim();
+            string b = Console.ReadLine().Trim();
+
+            int[] alphabets = new int[26];
+            foreach (char c in a) {
+                alphabets[c - 'a']++;
+            }
+
+            foreach (char c in b)
+            {
+                alphabets[c - 'a']--;
+            }
+
+            int count = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (alphabets[i] == 0) continue;
+                if (alphabets[i] < 0) count += (-alphabets[i]);
+                else count += alphabets[i];
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
